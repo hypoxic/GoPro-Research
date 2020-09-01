84fc0073df9b7b25080f5d5324150334c9e511d3
Trunk <Trunk@users.noreply.github.com>
2020-08-31 20:35:04 -0700

    Rebased for up and coming camera

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# GoPro HERO8 and GoPro MAX Research
-[GoPro HERO8 Teardown](https://gethypoxic.com/blogs/technical/gopro-hero8-teardown)   
-[GoPro HERO MAX Teardown](https://gethypoxic.com/blogs/technical/gopro-max-teardown)
+# GoPro Research
 
-* GoPro_Max directory for the device specific information   
-* tools works with both   
+This Repo has logs, utilities, and legally extracted assests for various GoPro Cameras. Older camera's assets are in their own unique repos within Hypoxic's Github.
+
+## Camera Teardowns 
+[Boilers Teardown - Coming Soon]()   
+[GoPro HERO MAX Physical Teardown](https://gethypoxic.com/blogs/technical/gopro-max-teardown)  
+[GoPro HERO8 Teardown](https://gethypoxic.com/blogs/technical/gopro-hero8-teardown)    
+[GoPro HERO7 Silver & White Research](https://gethypoxic.com/blogs/technical/gopro-hero7-silver-gopro-hero7-white-research)  
+[GoPro HERO7 Teardown](https://gethypoxic.com/blogs/technical/gopro-hero7-teardown) | [HERO7 Repo](https://github.com/hypoxic/gopro-HERO7)  
+[GoPro HERO6 Teardown](https://gethypoxic.com/blogs/technical/gopro-hero6-teardown-preliminary)  
+[GoPro HERO (2018 Research)](https://gethypoxic.com/blogs/technical/latest-gopro-hero-is-actually-a-gopro-hero5)  
+[GoPro HERO5 Physical Teardown](https://gethypoxic.com/blogs/technical/gopro-hero5-tear-down-and-software-study) | [HERO5 Repo](https://github.com/hypoxic/hero5)
+
+## Interfaces & Accessories
+[Micronesia (Security Enclave) Research](https://gethypoxic.com/blogs/technical/a-practical-guide-for-cracking-aes-128-encrypted-firmware-updates)  
+[GoPro Media Mod Teardown](https://gethypoxic.com/blogs/technical/gopro-media-mod-teardown)   
+[Anti Cloning Technology on the GoPro HEROBUS(tm) 2.0](https://gethypoxic.com/blogs/technical/anti-cloning-technology-on-the-gopro-herobus-2-0?_pos=2&_sid=d65b56eb9&_ss=r)  
+[GoPro HEROBus(tm) Research](https://gethypoxic.com/blogs/technical/gopro-herobus-for-gopro-hero5)  
+[GoPro HERO\[5-9\] Interfaces](https://gethypoxic.com/blogs/technical/gopro-hero5-interfaces)  
+
+
+## Tools 
+Various tools I use to extract data from the firmware images.   
+Works with GoPro HERO\[6-x\]
+*   **unpackh6** - Breaks up the firmware into it's various binary blobs
+*   **binldr.py** - IDA loader for RTOS straight from update file
+*   **busybox** - Compiled busybox with all the components such as telnetd 
+*   **decompress.py** - used to decompress Embedded Wizard content
+*   **extract_read8.bin.py** - Extracts the various binaries from a read image. Useful for research with variables in memory. 
+*   **extracth8dtb.py** - Extracts the Device tree from the image. Requires FdtBlobParse & glob
+*   **extract8rom.py** - Extracts the rom files from the HERO8/MAX image
+*   **extractXromfs.py** - Extracts the rom files from the HEROX image. Note, it's not precise as the memory size is now in code. A warning is given when the memory bounds do not match. 
+*   **extracthbmpd** - Extracts the display's bitmaps from front and back
+*   **gp1dropper** - rewrites memory in the rtos from linux for code execution in the the rtos
+*   **memdump_h8** - linux based utility to read all of the RTOS memory space and dump it to a file. Useful for research with global data initialized. 
+*   **readldr.py** - IDA loader for a read in binary. Use memdump_h8 within linux os, then use this loader on resulting binary.
+*   **replace_rom_file.py** - Replaces a rom file with an external rom file and recalculates the crcs. Must be the same size. Replace the original files with the resulting files
+*   **replace_section_file** - Replaces an extracted section with the one passed. Usefull for patching linux to add your own hook. 
+
+
---
+++ tools/readldr.py
@@ -0,0 +1,274 @@
+"""
+A script that extracts GoPro Binary
+
+The script uses very basic shellcode extraction algorithm
+
+Copyright (c) 2018 Hypoxic
+ALL RIGHTS RESERVED.
+
+Revision history
+=========================
+v1.0 - initial version
+
+"""
+
+import re
+import zlib
+import struct
+
+try:
+    import idaapi
+    from idc import *
+    ida = True
+except:
+    ida = False
+
+MAGIC_HDR = "MILBEAUT"
+BASE_ADDR = 0xA0000000
+END_ADDR  = 0xAA000000
+
+# -----------------------------------------------------------------------
+class volume(object):
+    """
+    @param vtype: volume type as read from the header
+    @param vid: volume id 
+    @param vsize: volume size minus header
+    #param buf: The volume's data 
+    """
+    def __init__(self, vtype, vid, vsize, buf):
+        self.vtype = vtype
+        self.vid = vid
+        self.size = vsize   
+        self.buf = buf   
+
+# -----------------------------------------------------------------------
+class parser:
+    """
+    Parse the Socionext Data.bin file
+
+    @param li: a file-like object which can be used to access the input data
+    @return: volume
+    """
+    def __init__(self,li):
+        self.size =  li.size()
+        li.seek(0)
+        self.f = li
+        
+    def __iter__(self):
+        return self
+
+#    def __next__(self):
+    def next(self):
+        try:
+            f = self.f
+            # Read the header
+            (magic,voltype, volid, volsize) = struct.unpack("<8sHHI", f.read(16))
+            
+            if magic != MAGIC_HDR:
+                StopIteration
+            
+            # Read the data
+            b = f.read(volsize)
+            
+            return(volume(voltype, volid, volsize,b))
+        except:
+            raise StopIteration
+  
+# -----------------------------------------------------------------------
+def accept_file(li, filename):
+    """
+    Check if the file is of supported format
+    @param li: a file-like object which can be used to access the input data
+    @param filename : filename
+    @return: 0 - no more supported formats
+             string "name" - format name to display in the chooser dialog
+             dictionary { 'format': "name", 'options': integer }
+               options: should be 1, possibly ORed with ACCEPT_FIRST (0x8000)
+               to
+    """   
+    retval = ""
+    
+    size = li.size()
+    b = li.read(size)    
+    daddr = b.find("PIQ01_01")
+    
+    if (b.find("gopro") != -1) and (daddr != -1): 
+        retval = "Updated GP1 Read Loader of size %d Data offset at:%x" % (size, daddr)
+
+    return retval
+
+
+def add_xrefs(addr, end=idc.BADADDR):
+    """
+        https://github.com/xyzz/vita-ida-physdump/blob/master/vita_phys_dump.py
+        Searches for MOV / MOVT pair, probably separated by few instructions,
+        and adds xrefs to things that look like addresses
+    """    
+    while addr < end and addr != BADADDR:
+        addr = idc.NextHead(addr)
+        if idc.GetMnem(addr) in ["MOV", "MOVW"]:
+            reg = idc.GetOpnd(addr, 0)
+            if idc.GetOpnd(addr, 1)[0] != "#":
+                continue
+            val = idc.GetOperandValue(addr, 1)
+            found = False
+            next_addr = addr
+            for x in range(16):
+                next_addr = idc.NextHead(next_addr)
+                if idc.GetMnem(next_addr) in ["B", "BX"]:
+                    break
+                # TODO: we could handle a lot more situations if we follow branches, but it's getting complicated
+                # if there's a function call and our register is scratch, it will probably get corrupted, bail out
+                if idc.GetMnem(next_addr) in ["BL", "BLX"] and reg in ["R0", "R1", "R2", "R3"]:
+                    break
+                # if we see a MOVT, do the match!
+                if idc.GetMnem(next_addr) in ["MOVT", "MOVT.W"] and idc.GetOpnd(next_addr, 0) == reg:
+                    if idc.GetOpnd(next_addr, 1)[0] == "#":
+                        found = True
+                        val += idc.GetOperandValue(next_addr, 1) * (2 ** 16)
+                    break
+                # if we see something other than MOVT doing something to the register, bail out
+                if idc.GetOpnd(next_addr, 0) == reg or idc.GetOpnd(next_addr, 1) == reg:
+                    break
+            if val & 0xFFFF0000 == 0:
+                continue
+            if found:
+                # pair of MOV/MOVT
+                try:
+                    idc.OpOffEx(addr, 1, idc.REF_LOW16, val, 0, 0)
+                    idc.OpOffEx(next_addr, 1, idc.REF_HIGH16, val, 0, 0)
+                except:
+                    print "Failed xref @ %x next_addr %x val %x" % (addr, next_addr, val)
+            else:
+                # a single MOV instruction
+                try:
+                    idc.OpOff(addr, 1, 0)
+                except:
+                    print "Failed xref at addr %x" % (addr)
+                    
+
+def MakeUnknownCode(start,end):   
+    """
+        Makes code in the data section that is not made yet
+        @param start Start address
+        @param start End address
+
+    """
+    addr = start
+    
+    while addr < end and addr != BADADDR:			
+        addr = find_unknown(addr, idaapi.SEARCH_DOWN)
+        if(addr % 4):
+        	addr = (addr & 0xFFFFFFFC)+4
+        	        
+        if isUnknown(GetFlags(addr)) and idc.Dword(addr) != 0:
+            #print "C 0x%08x" % addr
+            MakeCode(addr)
+    
+def MakeUnknownFncs(start, end):
+    """
+        Makes code that is not yet a function a function
+        @param start Start address
+        @param start End address
+
+    """
+    addr = start
+
+    while addr < end and addr != BADADDR:
+        if idc.Dword(addr) != 0 and isCode(GetFlags(addr)) and (idc.GetMnem(addr) != "NOP") and not (idaapi.get_func(addr)):
+            #print "N 0x%08x" % addr
+            idc.MakeFunction(addr, idaapi.BADADDR)        
+        addr = NextHead(addr, BADADDR)
+
+# -----------------------------------------------------------------------
+def load_file(li, neflags, format):
+
+    """
+    Load the file into database
+
+    @param li: a file-like object which can be used to access the input data
+    @param neflags: options selected by the user, see loader.hpp
+    @return: 0-failure, 1-ok
+    """
+    
+    # Select the PC processor module
+    idaapi.set_processor_type("ARM:ARMv8", SETPROC_ALL|SETPROC_FATAL)
+    inf = idaapi.get_inf_structure()
+    inf.af &= ~idc.AF_MARKCODE  # this is so that IDA does not find functions inside .data segments
+    inf.af2 &= ~idc.AF2_FTAIL  # don't create function tails
+    inf.af2 |= idc.AF2_PURDAT  # control flow to data segment is ignored 
+    
+    #idc.ChangeConfig('ARM_SIMPLIFY = NO')
+    start = BASE_ADDR
+
+    size = li.size()
+    li.seek(0)    
+    buf = li.read(size)
+    print("type of buf",type(buf))
+    daddr = buf.find("PIQ01_01")
+    
+    if ( daddr == -1 ):
+        print "Did not find PIQ01_01, leaving"
+        return -1
+    
+    print ("found data section starting at %x" % (daddr))
+
+    print "Assuming RTOS"
+    #end = start + size
+    end = END_ADDR
+    DATA_ADDR = start + daddr
+    
+    #Code Segment
+    print "1) Creating Code Section"
+    seg = idaapi.segment_t()
+    seg.start_ea = start
+    seg.end_ea   = DATA_ADDR
+    seg.bitness  = 1 # 32-bit
+    idaapi.add_segm_ex(seg, ".text", "CODE", idaapi.ADDSEG_OR_DIE)
+    
+    #Data Segment
+    print "2) Creating Data Section"
+    segdata = idaapi.segment_t()
+    
+    segdata.start_ea = DATA_ADDR
+    segdata.end_ea   = end
+    segdata.bitness  = 1 # 32-bit
+    idaapi.add_segm_ex(segdata, ".data", "DATA", idaapi.ADDSEG_OR_DIE)
+                
+    # now load the memory
+    print "3) Loading Memory"
+    idaapi.mem2base(buf, start, end)
+    
+    # Mark for analysis
+    print "4) First Pass Analalzying Code"
+    idaapi.add_entry(start, start, "start", 1)
+    plan_and_wait( seg.start_ea,seg.end_ea,True)
+    auto_wait()
+    
+    idc.Wait()
+    
+    print "5) Making unknown Code"
+    MakeUnknownCode(seg.start_ea,seg.end_ea)  
+
+    print "6) Making Functions"
+    MakeUnknownFncs(seg.start_ea,seg.end_ea)
+
+    print "7) Analyzing system instructions"
+    from highlight_arm_system_insn import run_script
+    run_script()
+
+    print "8) Adding MOVT/MOVW pair xrefs"
+    add_xrefs(seg.start_ea,seg.end_ea)
+    
+    print "9) Analyze Data Section"
+    plan_and_wait( segdata.start_ea, segdata.end_ea, True)
+    auto_wait()
+ 
+    return 1
+
+# -----------------------------------------------------------------------
+def main():
+    return 1
+
+if not ida:
+    main()
